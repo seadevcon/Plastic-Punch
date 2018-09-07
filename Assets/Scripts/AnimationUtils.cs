39460078062648926e40364100fc6636abec2df8
Christoph Graf <christoph.graf@gmx.at>
2018-09-08 00:59:00 +0200

    Added animation for hero

---
+++ Assets/Scripts/AnimationUtils.cs
@@ -0,0 +1,5 @@
+﻿public class AnimationUtils {
+
+    public static string ANIM_PARAM_ISMOVING = "IsMoving";
+    public static string ANIM_PARAM_HASSTOPPED = "HasStopped";
+}
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager instance = null;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+      
+    }
+
+    public void RegisterEvents()
+    {
+    }
+
+    public void UnregisterEvents()
+    { }
+
+}
