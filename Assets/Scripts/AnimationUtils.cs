88c97f4fa2a71900d17b446770b59af46482d39d
Christoph Graf <christoph.graf@gmx.at>
2018-09-08 01:28:05 +0200

    added punch animation for character

--- Assets/Scripts/AnimationUtils.cs
+++ Assets/Scripts/AnimationUtils.cs
@@ -2,4 +2,7 @@
 
     public static string ANIM_PARAM_ISMOVING = "IsMoving";
     public static string ANIM_PARAM_HASSTOPPED = "HasStopped";
+    public static string ANIM_PARAM_PUNCHLEFT = "PunchLeft";
+    public static string ANIM_PARAM_PUNCHRIGHT = "PunchRight";
+
 }
