de1152ffb192b0eb3145c4af6670c669657b6540
Les-Lee <les.dams96@gmail.com>
2018-09-08 06:48:17 +0200

    not funny anymore

---
+++ Assets/WaterPollution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WaterPollution : MonoBehaviour {
+    public float WaterHealth = 40;
+    public Image PollutionBar;
+    private float MaxWaterHealth;
+	// Use this for initialization
+	void Start () {
+        MaxWaterHealth = WaterHealth;
+        UpdatePollutionBar();
+	}
+	public void Pollution()
+    {
+        WaterHealth -= 1;
+        UpdatePollutionBar();
+    }
+    void UpdatePollutionBar()
+    {
+        PollutionBar.fillAmount = WaterHealth / MaxWaterHealth;
+    }
+
+}
