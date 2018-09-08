cafe67e527525f7672889a2cd573011864f12a06
Les-Lee <les.dams96@gmail.com>
2018-09-08 11:13:55 +0200

    mehr text

--- Assets/ShowResults.cs
+++ Assets/ShowResults.cs
@@ -9,12 +9,22 @@ public class ShowResults : MonoBehaviour {
     private int i = 0;
     private float interval = 0.1f;
     private bool finished = false;
-
     public GameObject image;
 
     // Use this for initialization
     void Start()
     {
+        string[] infos = new string[]{
+            "Around 70% of the Earth’s surface is covered by oceans.",
+            "The largest ocean on Earth is the Pacific Ocean, covering around 30% of the Earth’s surface.",
+            "The name “Pacific Ocean” comes form the Latin name Tepre Pacificum, “peaceful sea”.",
+            "The deepest known area of the Earth’s oceans is known as the Mariana Trench. It’s deepest point measures 11km. That’s a long dive down!",
+            "The world’s oceans are home to incredible creatures that are masters of disguise!",
+            "The longest mountain range in the world is found under water. Stretching over 56,000km, the Mid-Oceanic Ridge is a mountain chain that runs along the centre of the ocean basins.",
+            "About 70% of the oxygen we breathe is produced by the oceans.",
+            "The sea is home to the world’s largest living structure – the Great Barrier Reef. Measuring around 2,600km, it can even be seen from the Moon!",
+            "We have only explored about 5% of the world’s oceans. There’s a lot more to be discovered!",
+            "The sea can be described as the planet’s mega museum. There are more artefacts and remnants of history in the ocean than in all of the world’s museums combined!" };
         cups = PlayerPrefs.GetInt("killedTrash", 0);
         Debug.Log(cups);
         StartCoroutine(WriteText());
